36c20f62a2e78531c5387fb6b23a1968039987e0
ericmchen15 <146371252+ericmchen15@users.noreply.github.com>
2023-10-15 14:51:27 -0700

    Create ch.ts
    China surnames

---
+++ src/surnames/ch.ts
@@ -0,0 +1,9 @@
+const saudiSurnames = [ // Surnames - https://surnam.es/china
+    'Wang', 'Li', 'Zhang', 'Liu', 'Chen', 'Yang', 'Huang', 'Wu', 'Xu', 'Zhao',
+    'Zhou', "Lu', 'Zhu', 'Sun', 'Ma', 'He', 'Yu', 'Hu', 'Lin', 'Jiang',
+    'Guo', 'Luo', 'Gao', 'Zheng', 'Tang', 'Liang', 'Wei', 'Shi', 'Xie', ' Song',
+    'Feng', 'Yan', "Han', 'Deng', 'Cao', 'Tan', 'Ceng', 'Peng', 'Xiao', 'Yuan',
+    'Pan', 'Cheng', 'Cai', 'Fan', 'Tian', 'Dong', 'Fu', 'Ye', 'Su', 'Du'
+];
+
+export default chineseSurnames;
