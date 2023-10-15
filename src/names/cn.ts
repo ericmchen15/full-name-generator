6f86afe89b914c8e68e23c2970e3788f197bf80f
ericmchen15 <146371252+ericmchen15@users.noreply.github.com>
2023-10-15 14:37:26 -0700

    Create cn.ts
    List of 50 Chinese names for male and females

---
+++ src/names/cn.ts
@@ -0,0 +1,20 @@
+const chineseNames = {
+    0: [ // Male names - https://www.pampers.com/en-us/pregnancy/baby-names/article/chinese-boy-names
+        'Mùchén', 'Hàoyú', 'Mùchén', 'Míngzé', 'Yìchén', 'Yúzé', 'Hàorán', 'Yìzé', 'Yŭxuān', 'Mùyáng',
+        'Bo', 'Da', 'De', 'Fu', 'Gang', 'Guo', 'Hao', 'Haoyu', 'Jun Hie', 'Qian'
+        'Aiguo', 'Bolin', 'Chaoxiang', 'Genghis', 'Jaw-Long', 'Shoi-Ming', 'Xiaobo', 'Xiaosheng', 'Zimo', 'Zixin',
+        'Chang', 'Chen', "Dong', 'Dongyang', 'Hong', 'Jiahao', 'Jianhong', 'Jun', 'Long', 'Qiang',
+        'Chin', 'Quanting', 'Junfeng', 'Kai', 'Lin', 'Lingyun', 'Quon', 'Tao', 'Tu', 'Wen'
+    ],
+    1: [ // Female names - https://www.20000-names.com/female_chinese_names.htm
+        'Ai', 'Baozhai', 'Biyu', 'Changchang', 'Changying', 'Chen', 'Chenguang', 'Chunhua', 'Chuntao', 'Cuifen',
+        'Daiyu', 'Dandan', 'Dongmei', 'Ehuang', 'Fan', 'Fang', 'Fenfang', 'Guang', 'Hong', Hualing',
+        'Huan', 'Huian', 'Huifang', 'Huiling', 'Huiqing', 'Jia', 'Jiao', 'Jiayi', "Jiaying', 'Jie',
+        'Jingfei', 'Jinghua', 'Jinjing', 'Ju', 'Juan', 'Lan', 'Lanfen', 'Lanying', 'Li', 'Lifen',
+        'Lihua', 'Lijuan', 'Liling', 'Lin', 'Ling', 'Liqin', 'Liqiu', 'Liu', 'Luli', 'Mei'
+    ]
+}
+
+
+
+export default chineseNames;
